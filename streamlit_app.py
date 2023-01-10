4c9ebc98ebd2f61a559d4e920d6566f95c0198ab
gemalada-acn <88875882+gemalada-acn@users.noreply.github.com>
2023-01-10 18:07:58 +0800

    Update streamlit_app.py
    removed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,8 +64,6 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
-streamlit.dataframe(my_data_rows)
-
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
